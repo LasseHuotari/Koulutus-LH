7e33ebcf45aae1ec5180f72a96f535797eae8974
lasse.huotari <lassehuotari@hotmail.com>
2019-07-31 15:31:42 +0300

    Create filereader.js

---
+++ Harjoitukset/filereader.js
@@ -0,0 +1,13 @@
+
+var txtFile = new XMLHttpRequest();
+var allText = "file not found";
+txtFile.onreadystatechange = function () {
+  if (txtFile.readyState === XMLHttpRequest.DONE && txtFile.status == 200) {
+    allText = txtFile.responseText;
+    allText = allText.split("\n").join("<br>");
+  }
+
+  document.getElementById('txt').innerHTML = allText;
+}
+txtFile.open("GET", 'bu.txt', true);
+txtFile.send(null);
